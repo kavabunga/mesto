5759805bb2dcd959bd1ee12615d29e58179cb7af
kavabunga <kavabunga@gmail.com>
2022-08-16 01:23:22 +0300

    fix: review-01

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,28 @@ let nameElement = document.querySelector('.profile__name');
 let occupationElement = document.querySelector('.profile__occupation');
 let popupElement = document.querySelector('.popup');
 let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('#input-name');
-let occupationInput = document.querySelector('#input-occupation');
+let nameInput = document.querySelector('.form__input_name_name');
+let occupationInput = document.querySelector('.form__input_name_occupation');
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
 
 let popupToggleClass = 'popup_opened';
 
-let user = nameElement.textContent;
-let occupation = occupationElement.textContent;
-
 function popupToggle() {
   let toggleClass = popupToggleClass;
-  if (!!popupElement.classList.contains(toggleClass)) {
+  if (popupElement.classList.contains(toggleClass)) {
     popupElement.classList.remove(toggleClass);
   } else {
-    nameInput.value = user;
-    occupationInput.value = occupation;
+    nameInput.value = nameElement.textContent;
+    occupationInput.value = occupationElement.textContent;
     popupElement.classList.add(toggleClass);
   }
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    user = nameInput.value;
-    occupation = occupationInput.value;
-    nameElement.textContent = user;
-    occupationElement.textContent = occupation;
+    nameElement.textContent = nameInput.value;
+    occupationElement.textContent = occupationInput.value;
     popupToggle();
 }
 
